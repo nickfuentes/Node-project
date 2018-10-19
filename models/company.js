1e27cefec5f95d07692344e56c38d5300e7a55da
Nickolas Fuentes <>
2018-10-19 16:58:52 -0500

    Created models for Company and Employee and created relationship between both, also created Company controller

--- models/company.js
+++ models/company.js
@@ -1,10 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Company = sequelize.define('Company', {
-    name: DataTypes.STRING
-  }, {});
-  Company.associate = function(models) {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  Company.associate = function (models) {
     // associations can be defined here
+    Company.hasMany(models.Employee, {
+      foreignKey: 'companyId',
+      as: 'employees',
+    });
   };
   return Company;
 };
